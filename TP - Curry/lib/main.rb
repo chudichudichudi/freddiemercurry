f4a9e984e5e1bb1f18ace4b2f6278c9195fb2c1c
Andres Navarro <canavarro82@gmail.com>
2016-04-17 15:48:18 -0300

    Agregue una simulacion basica.  Para la 1er user story de armar la simulacion

--- TP - Curry/lib/main.rb
+++ TP - Curry/lib/main.rb
@@ -2,4 +2,12 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-puts "Hello World"
+## Habría que ir organizando tests de verdad... (Andrés)
+require "jugador.rb"
+require "equipo.rb"
+require "simulacion_en_curso.rb"
+
+e1 = Equipo.new()
+e2 = Equipo.new()
+simulacion = SimulacionEnCurso.new(e1, e2)
+print simulacion
