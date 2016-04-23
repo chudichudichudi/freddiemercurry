739f1798a82ce6e807b13347b43055cc54204ae4
Alumno <alumno@huayra.(none)>
2016-04-23 16:22:49 -0300

    zoi ezpezial

---
+++ tp_curry/lib/simulacion.rb
@@ -0,0 +1,29 @@
+# encoding: UTF-8
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Simulacion
+  attr_reader :equipoDesafiante, :equipoDesafiado,
+      :puntajeDesafiante, :puntajeDesafiado
+  
+   
+  def initialize(equipoDesafiante, equipoDesafiado)
+    @equipoDesafiante = equipoDesafiante
+    @equipoDesafiado = equipoDesafiado
+    # Una simulación siempre empieza 0-0
+    @puntajeDesafiante = @puntajeDesafiado = 0
+  end
+  
+  def to_s()
+    return "Simulación entre: " + equipoDesafiante.to_s() + " y " +
+      equipoDesafiado.to_s() + ".  Puntaje: " + @puntajeDesafiante.to_s() +
+      "-" + @puntajeDesafiado.to_s()
+  end
+  
+  @equipoDesafiante
+  @equipoDesafiado
+  @estado
+
+end
+
