e74060977f20787a9b7ce92dd7c1ba9c338beb58
Alumno <alumno@huayra.(none)>
2016-04-23 16:15:13 -0300

    arrancando la simulación

--- tp_curry/lib/simulacion_en_curso.rb
+++ tp_curry/lib/simulacion_en_curso.rb
@@ -3,8 +3,30 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-class SimulacionEnCurso
+
+class Estado
   attr_reader :equipoDesafiante, :equipoDesafiado,
+      :puntajeDesafiante, :puntajeDesafiado
+  
+   
+  def initialize
+  end
+  
+  def to_s()
+    return "Simulación entre: " + equipoDesafiante.to_s() + " y " +
+      equipoDesafiado.to_s() + ".  Puntaje: " + @puntajeDesafiante.to_s() +
+      "-" + @puntajeDesafiado.to_s()
+  end
+  
+	def siguienteTurno
+	end
+
+
+end
+
+
+class EnCurso<Estado
+	attr_reader :equipoDesafiante, :equipoDesafiado,
       :puntajeDesafiante, :puntajeDesafiado
   
    
@@ -27,4 +49,27 @@ class SimulacionEnCurso
   @puntajeDesafiado
 end
 
+class Terminado<Estado
+	attr_reader :equipoDesafiante, :equipoDesafiado,
+      :puntajeDesafiante, :puntajeDesafiado
+  
+   
+  def initialize(equipoDesafiante, equipoDesafiado)
+    @equipoDesafiante = equipoDesafiante
+    @equipoDesafiado = equipoDesafiado
+    # Una simulación siempre empieza 0-0
+    @puntajeDesafiante = @puntajeDesafiado = 0
+  end
+  
+  def to_s()
+    return "Simulación entre: " + equipoDesafiante.to_s() + " y " +
+      equipoDesafiado.to_s() + ".  Puntaje: " + @puntajeDesafiante.to_s() +
+      "-" + @puntajeDesafiado.to_s()
+  end
+  
+  @equipoDesafiante
+  @equipoDesafiado
+  @puntajeDesafiante
+  @puntajeDesafiado
+end
 
